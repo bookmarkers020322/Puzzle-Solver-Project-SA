50d1429e26a782b9a189cbe6a2eb64e384536722
rakaalfryan111 <dominicusalfryan111@gmail.com>
2018-06-28 07:20:11 +0700

    Update Swap_puzzle.java

--- Swap_puzzle.java
+++ Swap_puzzle.java
@@ -2,6 +2,7 @@ package swap_puzzle;
 import java.util.Scanner;
 public class Swap_puzzle {
     public  static int board [][] = new int [3][3];
+    public static int simpan[]=new int[2];
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int angka [] = new int [9];
@@ -30,6 +31,32 @@ public class Swap_puzzle {
             y=0;
             System.out.println("");
             x++;
+        }
+    }
+    public static void cari(){
+        int a=0, x=0, y=0,target=1;
+        while(a<100){
+            while(x<3){
+                while(y<3){
+                    if(board[x][y]==target){
+                        simpan[0] = x;
+                        simpan[1] = y;
+                        System.out.println("posisi "+target+" ada di koordinat "+simpan[0]+" dan "+simpan[1]);
+                        target++;
+                        y=3;
+                        x=3;
+                    }
+                    y++;
+                }
+                y=0;
+                //System.out.println("");
+                x++;
+            }
+            
+            a++;
+            x=0;
+            //System.out.println(target);
+            //System.out.print(".");
         }
     }
     
