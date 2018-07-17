1c095f4a48db8d9c35116a65ab1e435071334ce5
antonius30 <40001299+antonius30@users.noreply.github.com>
2018-07-17 15:30:48 +0700

    Update tools.java

--- tools.java
+++ tools.java
@@ -1,13 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package swap_puzzle;
-/**
- *
- * @author raka
- */
 
 public class tools {
     public static int board [][] = new int [3][3];
@@ -76,22 +67,7 @@ public class tools {
             ix++;
         }else{iy++;}
         
-//        int pindah,i=0,ix=0,iy=0;
-//        while(ix<3){
-//            while(iy<3){
-//                
-//                iy++;   
-//            }
-//            while(x==ix){
-//                
-//            }
-//            ix++;
-//            while(y==0){
-//                
-//            }
-//            iy++;
-//            ix++;
-//        }   
+
     }
     
 }
